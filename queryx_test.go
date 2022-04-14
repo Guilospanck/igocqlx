f9f57f42344e97bbd10f555ca4d698deebe0dd3c
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-04-14 20:09:17 -0300

    test: queryx_test

--- queryx_test.go
+++ queryx_test.go
@@ -9,45 +9,98 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type TrackingDataEntity struct {
+	FirstName string `db:"first_name"`
+	LastName  string `db:"last_name"`
+	Location  string `db:"location"`
+}
+
+func makeArg() *TrackingDataEntity {
+	return &TrackingDataEntity{
+		FirstName: "Guilherme",
+		LastName:  "Rodrigues",
+		Location:  "Brazil",
+	}
+}
+
+func makeArgArrayInterface() []interface{} {
+	return []interface{}{
+		"Guilherme",
+		"Rodrigues",
+		"Brazil",
+	}
+}
+
 func GetUnexportedField(field reflect.Value) interface{} {
 	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
 }
 
 func Test_Queryx_WithBindTransformer(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("Should call WithBindTransformer and perform right actions", func(t *testing.T) {
 		// arrange
 		sut := makeQueryxStruct()
 
 		// act
 		result := sut.query.WithBindTransformer(sut.tr)
 
+		/* Getting private field from struct */
 		queryInsideResult := result.(*Queryx).Q
-		unexportedTr := GetUnexportedField(reflect.ValueOf(queryInsideResult).Elem().FieldByName("tr"))
+		field := reflect.ValueOf(queryInsideResult).Elem().FieldByName("tr")
+		unexportedTr := GetUnexportedField(field)
 
 		// assert
 		assert.IsType(t, sut.tr, unexportedTr)
 		assert.Equal(t, sut.query, result)
-		assert.Equal(t, result.(*Queryx).Q, sut.gocqlx_query)
+	})
+}
+
+// https://github.com/scylladb/gocqlx/blob/master/queryx_test.go
+func Test_Queryx_BindStruct(t *testing.T) {
+	t.Run("Should call Test_Queryx_BindStruct and perform right actions", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxStruct()
+		arg := makeArg()
+
+		// act
+		result, err := sut.query.bindStructArgs(arg, nil)
+
+		// assert
+		assert.NoError(t, err)
+		assert.Equal(t, makeArgArrayInterface(), result)
 	})
 }
 
 type queryxStruct struct {
-	gocqlx_query *gocqlx.Queryx
-	query        *Queryx
-	tr           gocqlx.Transformer
+	query       *Queryx
+	gocqlxquery *gocqlx.Queryx
+	tr          gocqlx.Transformer
+
+	stmt  string
+	names []string
 }
 
 func makeQueryxStruct() queryxStruct {
-	gocqlx_query := &gocqlx.Queryx{}
-	query := &Queryx{
-		Q: gocqlx_query,
-	}
+	tr := func(name string, val interface{}) interface{} { return val }
 
-	tr := func(name string, val interface{}) interface{} { return nil }
+	stmt := `INSERT INTO tracking_data (first_name,last_name,location) VALUES (?,?,?) `
+	names := []string{"first_name", "last_name", "location"}
+
+	query := makeQuery(names)
+	gocqlxquery := query.Q
 
 	return queryxStruct{
-		gocqlx_query,
 		query,
+		gocqlxquery,
 		tr,
+		stmt,
+		names,
+	}
+}
+
+func makeQuery(names []string) *Queryx {
+	gocqlxquery := gocqlx.Query(nil, names)
+
+	return &Queryx{
+		Q: gocqlxquery,
 	}
 }
